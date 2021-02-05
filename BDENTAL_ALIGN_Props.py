6c0d711e0244b43d2fe916b099bbc210ee403153
Dr issam Dakir <nevermindnach@gmail.com>
2021-02-05 11:33:47 +0100

    Update BDENTAL_ALIGN_Props.py

--- BDENTAL_ALIGN_Props.py
+++ BDENTAL_ALIGN_Props.py
@@ -66,45 +66,4 @@ def unregister():
     del bpy.types.Scene.BDENTAL_ALIGN_Props
 
 
-# props examples :
-
-# Axial_Loc: FloatVectorProperty(
-#     name="AXIAL location",
-#     description="AXIAL location",
-#     subtype="TRANSLATION",
-#     update=AxialSliceUpdate,
-# )
-# Axial_Rot: FloatVectorProperty(
-#     name="AXIAL Rotation",
-#     description="AXIAL Rotation",
-#     subtype="EULER",
-#     update=AxialSliceUpdate,
-# )
-################################################
-# Str_Prop_Search_1: StringProperty(
-#     name="String Search Property 1",
-#     default="",
-#     description="Str_Prop_Search_1",
-# )
-# Float Props :
-#########################################################################################
-
-# F_Prop_1: FloatProperty(
-#     description="Float Property 1 ",
-#     default=0.0,
-#     min=-200.0,
-#     max=200.0,
-#     step=1,
-#     precision=1,
-#     unit="NONE",
-#     update=None,
-#     get=None,
-#     set=None,
-# )
-#########################################################################################
-# # FloatVector Props :
-#     ##############################################
-#     FloatV_Prop_1: FloatVectorProperty(
-#         name="FloatVectorProperty 1", description="FloatVectorProperty 1", size=3
-#     )
-#########################################################################################
+#####################################
